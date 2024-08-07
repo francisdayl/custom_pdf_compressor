278d1464a617c88df948b7bc048f071a1671d674
David <david_yanez99@outlook.es>
2024-08-07 17:11:38 -0500

    responsive layout on index and redirection

--- app.py
+++ app.py
@@ -44,5 +44,129 @@ def compress_pdf():
     return render_template("compress.html")
 
 
+@app.route("/merge-pdf", methods=["GET", "POST"])
+def merge_pdf():
+    if request.method == "POST":
+        if "file" not in request.files:
+            return "No file uploaded", 400
+        file = request.files["file"]
+        if file.filename == "":
+            return "No file selected", 400
+        if file:
+            compression_type = request.form.get("compression_type", "Light")
+            expected_size = (
+                float(request.form.get("customSize")) * 10**6
+                if request.form.get("customSize")
+                else 2_000_000
+            )
+            file_name = file.filename
+            buffer: BytesIO = send_to_compress(
+                file_stream=file.stream,
+                compression_type=compression_type,
+                expected_size=expected_size,
+            )
+            buffer.seek(0)
+            return send_file(
+                buffer,
+                mimetype="application/pdf",
+                as_attachment=True,
+                download_name=f"{file_name[:-4]}_compressed.pdf",
+            )
+    return render_template("merge.html")
+
+
+@app.route("/extract-pdf", methods=["GET", "POST"])
+def extract_pdf():
+    if request.method == "POST":
+        if "file" not in request.files:
+            return "No file uploaded", 400
+        file = request.files["file"]
+        if file.filename == "":
+            return "No file selected", 400
+        if file:
+            compression_type = request.form.get("compression_type", "Light")
+            expected_size = (
+                float(request.form.get("customSize")) * 10**6
+                if request.form.get("customSize")
+                else 2_000_000
+            )
+            file_name = file.filename
+            buffer: BytesIO = send_to_compress(
+                file_stream=file.stream,
+                compression_type=compression_type,
+                expected_size=expected_size,
+            )
+            buffer.seek(0)
+            return send_file(
+                buffer,
+                mimetype="application/pdf",
+                as_attachment=True,
+                download_name=f"{file_name[:-4]}_compressed.pdf",
+            )
+    return render_template("extract.html")
+
+
+@app.route("/convert-pdf-jpg", methods=["GET", "POST"])
+def convert_pdf_to_jpg():
+    if request.method == "POST":
+        if "file" not in request.files:
+            return "No file uploaded", 400
+        file = request.files["file"]
+        if file.filename == "":
+            return "No file selected", 400
+        if file:
+            compression_type = request.form.get("compression_type", "Light")
+            expected_size = (
+                float(request.form.get("customSize")) * 10**6
+                if request.form.get("customSize")
+                else 2_000_000
+            )
+            file_name = file.filename
+            buffer: BytesIO = send_to_compress(
+                file_stream=file.stream,
+                compression_type=compression_type,
+                expected_size=expected_size,
+            )
+            buffer.seek(0)
+            return send_file(
+                buffer,
+                mimetype="application/pdf",
+                as_attachment=True,
+                download_name=f"{file_name[:-4]}_compressed.pdf",
+            )
+    return render_template("pdf_to_jpg.html")
+
+
+@app.route("/convert-jpg-pdf", methods=["GET", "POST"])
+def convert_jpg_to_pdf():
+    if request.method == "POST":
+        if "file" not in request.files:
+            return "No file uploaded", 400
+        file = request.files["file"]
+        if file.filename == "":
+            return "No file selected", 400
+        if file:
+            compression_type = request.form.get("compression_type", "Light")
+            expected_size = (
+                float(request.form.get("customSize")) * 10**6
+                if request.form.get("customSize")
+                else 2_000_000
+            )
+            file_name = file.filename
+            buffer: BytesIO = send_to_compress(
+                file_stream=file.stream,
+                compression_type=compression_type,
+                expected_size=expected_size,
+            )
+            buffer.seek(0)
+            return send_file(
+                buffer,
+                mimetype="application/pdf",
+                as_attachment=True,
+                download_name=f"{file_name[:-4]}_compressed.pdf",
+            )
+    return render_template("jpg_to_pdf.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5100)
