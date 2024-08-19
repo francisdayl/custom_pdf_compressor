65c9d81496730a9c34c516922379a6539fcabced
David <david_yanez99@outlook.es>
2024-08-19 13:32:07 -0500

    update exposed port

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-EXPOSE 5100
+EXPOSE 8000
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -145,4 +145,4 @@ def convert_jpg_to_pdf():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5100)
+    app.run(host="0.0.0.0", debug=True, port=8000)
