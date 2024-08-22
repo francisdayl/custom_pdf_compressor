dca17de0b0404c5a86c683896adf209dcc000493
David <david_yanez99@outlook.es>
2024-08-22 12:33:33 -0500

    add scalene profiler

---
+++ profiling/profiling.py
@@ -0,0 +1,7 @@
+import os
+from utils.pdf_utils import *
+
+
+send_to_compress("prueba.pdf", "Light", 10_000_000)
+send_to_compress("prueba.pdf", "Medium", 10_000_000)
+send_to_compress("prueba.pdf", "Extreme", 10_000_000)
